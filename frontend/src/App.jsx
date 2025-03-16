93a93f30033a5bfe4916bcf85beb886834eefbe9
siddesh <your-email@example.com>
2025-03-16 15:14:42 +0530

    sidebar is created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import SignupPage from './pages/SignupPage'
+import LoginPage from './pages/LoginPage'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import DashboardPage from './pages/DashboardPage'
+import Productid from './pages/Productid'
+import Sidebar from './pages/Sidebar'
 
 function App() {
   return (
     <>
-      <DashboardPage element={<DashboardPage/>} />
+    <Router>
+      <Routes>
+        <Route path='/' element={<SignupPage/>}></Route>
+        <Route path='/login' element={<LoginPage/>}></Route>
+        <Route path='/Dashboard' element={<DashboardPage/>}></Route>
+        <Route path='/Product' element={<Productid/>}></Route>
+        <Route path='/sidebar' element={<Sidebar/>}></Route>
+
+
+        
+      </Routes>
+    </Router>
     </>
   )
 }
 
-export default App
+export default App
---
+++ frontend/src/pages/Productid.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Productid() {
+  return (
+    <div className='flex flex-col min-h-screen bg-gradient-to-br from-black via-blue-950 to-black p-6 items-center justify-center'>
+        <div className='flex items-center justify-center border-2 w-5xl h-5xl bg-gray-900 rounded-lg'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Productid

372adb813d6b604606cf04314336cd5728428930
Suvesh Pagam <156836161+Suvesh0707@users.noreply.github.com>
2025-04-03 20:22:01 +0530

    deploy backend

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.post("/register", registerUser)
 router.post("/login", loginUser)
 router.post("/logout", logoutUser)
-router.post("/checkauth",protectRoute,checkAuth)
+router.get("/checkauth",protectRoute,checkAuth)
 
 export default router
